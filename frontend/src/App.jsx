33149236c75de6219cd5c6c1be5c0e951d8cd655
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-27 16:11:17 +0530

    all commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Login />
 
     </div>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,136 @@
+import { Menu, School } from 'lucide-react'
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import DarkMode from '@/pages/DarkMode'
+import { Separator } from '@radix-ui/react-dropdown-menu'
+
+
+const Navbar = () => {
+
+  //predefault()
+  const user = true;
+
+  return (
+    <div className='h-16 dark:bg-[#0A0A0A]b bg-white border-b dark:border-b-gray-800 border-b-gray-200 
+    fixed top-0 left-0 right-0 duration-300 z-10
+    '>
+      {/* DeskTop */}
+
+      <div className=" h-full w-full mx-auto container  hidden md:flex justify-between items-center gap-10">
+        <div className="flex items-center gap-5">
+          <School size={"30"} />
+          <h1
+            className='hidden md:block font-extrabold text 2xl'
+          >LMS-Learning</h1>
+        </div>
+        {/* User icons and dark mode icons */}
+        <div className="flex items-center gap-8">
+          {
+            user ? (<DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    My Learning
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    Eidt Profile
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  Log out
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Dashboard
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>)
+              :
+              <div className="flex items-center gap-2">
+                <Button>Login</Button>
+                <Button>Signup</Button>
+              </div>
+          }
+          <DarkMode />
+        </div>
+      </div>
+
+      {/* Mobile divice */}
+      <div className="flex md:hidden items-center justify-between px-4 h-full">
+        <h1 className='text-2xl font-bold'>LMS-Learning</h1>
+        <MobileNabar />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
+
+const MobileNabar = () => {
+
+  const role = "instructor"
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            size="icon"
+            className="rounded-full bg-gray-200 hover:bg-gray-200"
+            variant="outline"><Menu /></Button>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col">
+          <SheetHeader className="flex flex-row items-center justify-between mt-5">
+            <SheetTitle>LMS-Learning</SheetTitle>
+            <DarkMode />
+          </SheetHeader>
+          <Separator className='mr-2'>
+            <nav className='flex flex-col space-y-4 p-5'>
+              <span>My Leaning</span>
+              <span>Edit Profile</span>
+              <p>Logout</p>
+            </nav>
+            {
+              role === "instructor" && (<SheetFooter>
+                <SheetClose asChild>
+                  <Button type="submit">Save changes</Button>
+                </SheetClose>
+              </SheetFooter>)
+            }
+          </Separator>
+        </SheetContent>
+      </Sheet>
+
+    </div>
+  )
+} 
