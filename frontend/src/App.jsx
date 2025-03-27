538e0cef6f4ae4e3c7413d1d2777c66ce7ae1cf9
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-27 21:06:02 +0530

    routing structure all routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './App.css'
-import Login from './pages/Login'
-import Navbar from './components/Navbar'
-import HeroSection from './pages/student/HeroSection'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './Layout/MainLayout';
+import HeroSection from './pages/student/HeroSection';
+import Login from './pages/Login';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <Login />
+    <Router>
+      <Routes>
+        {/* Wrap pages that need layout inside MainLayout */}
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HeroSection />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
-    </div>
-  )
-}
-
-export default App
+export default App;
