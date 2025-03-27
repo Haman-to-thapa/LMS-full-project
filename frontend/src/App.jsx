8674596af8f2fc1362536eb49cadd1dbafc5db9c
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-27 20:37:35 +0530

    HeroSection add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
+import HeroSection from './pages/student/HeroSection'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <HeroSection />
       <Login />
 
     </div>
