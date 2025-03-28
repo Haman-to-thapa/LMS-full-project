b402e79cece179c3ca5b0ea866021213eb8bf724
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-28 16:44:39 +0530

    routes add login my-learning profile on routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './Layout/MainLayout';
-import HeroSection from './pages/student/HeroSection';
 import Login from './pages/Login';
+import LayoutUi from './Layout/HomeLayoutroutes';
+import MyLearning from './pages/student/MyLearning';
+import Profile from './pages/student/Profile';
+
 
 const App = () => {
   return (
@@ -10,8 +13,10 @@ const App = () => {
       <Routes>
         {/* Wrap pages that need layout inside MainLayout */}
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<HeroSection />} />
+          <Route index element={<LayoutUi />} />
           <Route path="login" element={<Login />} />
+          <Route path="my-learning" element={<MyLearning />} />
+          <Route path='profile' element={<Profile />} />
         </Route>
       </Routes>
     </Router>
