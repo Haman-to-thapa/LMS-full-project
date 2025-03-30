36448e9ff978d90a1194b558723613f73102cc47
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-03-30 16:28:00 +0530

    add getUserProfile req.id

--- backend/server/controllers/userController.js
+++ backend/server/controllers/userController.js
@@ -95,3 +95,30 @@ export const getUserProfile = async(req, res) => {
     return res.status(500).json({success:true, message:"Server Error"})
   }
 }
+
+
+
+export const updateProfile = async (req, res) => {
+
+  try {
+    const userId = req.id;
+    const {name} = req.body;
+    // this comming from mongodb
+    const profilePhoto = req.file;
+    
+    const user = await User.findById(userId);
+    if(!user) {
+      return res.status(404).json({success:false, message:"User not found"})
+    }
+
+
+
+    
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      success:false,
+      message:"Failed to update profile"
+    })
+  }
+}
