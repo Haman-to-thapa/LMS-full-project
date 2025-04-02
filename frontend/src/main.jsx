37f4693bc2409ae35a6f44ee5046dedf44e02242
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-02 09:59:59 +0530

    add custom to add loading ternarity oparity to showng loading or not

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './appRedux/store'
+import { store } from './appRedux/store'
 import { Toaster } from "@/components/ui/sonner"
+import { useLoadUserQuery } from './featureSlice/api/authApi'
+import InfinityLoader from './components/ui/LoadingSpinner'
+
+const Custom = ({ children }) => {
+  const { isLoading } = useLoadUserQuery();
+
+  return <>
+    {
+      isLoading ? <h1><InfinityLoader /></h1> : <>{children}</>
+    }
+  </>
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Custom>
+        <App />
+      </Custom>
       <Toaster />
     </Provider>
 
