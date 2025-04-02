0ecee93f97ce1e6f324fa97b056fd747beedd4fb
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-02 10:04:23 +0530

    some function will be add like onQueryStarted and dispatch to dont refresh authenication don

--- frontend/src/featureSlice/api/authApi.js
+++ frontend/src/featureSlice/api/authApi.js
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import { userLoggedIn } from './authSlice'; 
+import { userLoggedIn, userLoggedOut } from './authSlice'; 
 
 const USER_API="http://localhost:8080/api/v1/user/"
 
@@ -32,11 +32,31 @@ export const authApi = createApi({
           }
     }
   }),
+  logoutUser: builder.mutation({
+ query: () => ({
+  url:"logout",
+  method:"GET"
+ }), async onQueryStarted(_,{queryFulfilled, dispatch}) {
+  try {
+    const result = await queryFulfilled;
+    dispatch(userLoggedOut())
+  } catch (error) {
+    console.log(error)
+  }
+ }
+  }),
   loadUser: builder.query({
     query: () => ({
       url:"profile",
       method:"GET"
-    })
+    }), async onQueryStarted(_, {queryFulfilled, dispatch}) {
+      try {
+        const result = await queryFulfilled; 
+        dispatch(userLoggedIn({user:result.data.user}))
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }),
   updateProfileUser: builder.mutation({
     query:(formData) => ({
@@ -51,4 +71,4 @@ export const authApi = createApi({
 
 })
 
-export const {useRegisterUserMutation, useLoginUserMutation, useLoadUserQuery} = authApi;
+export const {useRegisterUserMutation, useLoginUserMutation, useLogoutUserMutation, useLoadUserQuery, useUpdateProfileUserMutation} = authApi;
