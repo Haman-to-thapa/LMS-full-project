70f1abb8ec6d7620458e1266099639ebf9aaa107
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-03 12:46:02 +0530

    add navigate

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
 
   return (
     <div className='h-16 dark:bg-[#0A0A0A]b bg-white border-b dark:border-b-gray-800 border-b-gray-200 
-    fixed top-0 left-0 right-0 duration-300 z-10
+    fixed top-0 left-0 right-0  z-10
     '>
       {/* DeskTop */}
 
