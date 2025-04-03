21e6223e9013b2fe712b39678addc32dd58f7acd
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-03 13:35:42 +0530

    add createCourse and also getCreatorCourse and also add tagTypes in courseApi

--- frontend/src/featureSlice/api/courseApi.js
+++ frontend/src/featureSlice/api/courseApi.js
@@ -1,8 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-const COURSE_API = "http//localhost:8080/api/v1/course"
+const COURSE_API = "http://localhost:8080/api/v1/course";
 
 export const courseApi = createApi({
   reducerPath:"courseApi",
+  tagTypes:['Refetch_Creator_Course'],
   baseQuery:fetchBaseQuery({
     baseUrl:COURSE_API,
     credentials:"include"
@@ -10,13 +11,21 @@ export const courseApi = createApi({
   endpoints: (builder) => ({
     createCourse: builder.mutation({
       query: ({courseTitle, category}) => ({
-        url:"/",
+        url:"",
         method:"POST",
         body:{courseTitle, category},
-      })
+      }),
+      invalidatesTags:['Refetch_Creator_Course']
+    }),
+    getCeatorCourse: builder.query({
+      query:() => ({
+        url:"",
+        method:"GET",
+      }),
+      providesTags:['Refetch_Creator_Course']
     })
   })
 })
 
 
-export const {useCreateCourseMutation} = courseApi;
+export const {useCreateCourseMutation, useGetCeatorCourseQuery} = courseApi;
