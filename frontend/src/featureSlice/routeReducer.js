ac5b9c8a3d0fc2996b8f1b77d76d7d37c24317c8
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-03 13:37:08 +0530

    add couresApi on add on rootReducer

--- frontend/src/featureSlice/routeReducer.js
+++ frontend/src/featureSlice/routeReducer.js
@@ -1,9 +1,11 @@
 import  {combineReducers} from '@reduxjs/toolkit'
 import { authApi } from '../featureSlice/api/authApi'
 import authReducer from '../featureSlice/api/authSlice'
+import { courseApi } from './api/courseApi';
 
 const rootReducer = combineReducers({
   [authApi.reducerPath]:authApi.reducer,
+  [courseApi.reducerPath]:courseApi.reducer,
   auth:authReducer
 })
 
