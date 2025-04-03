159854daa0d01dfe6d5ed4b72b52707ffae41863
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-03 12:40:43 +0530

    new Get routes added ot add all creatore courses see in admin

--- backend/routes/courseRoute.js
+++ backend/routes/courseRoute.js
@@ -1,10 +1,11 @@
 
 import express from 'express'
 import isAuthenticated from '../server/middleware/isAuthenticated.js';
-import { createCourse } from '../server/controllers/courseController.js';
+import { createCourse, getCreatorAllCourse } from '../server/controllers/courseController.js';
 
 const router = express.Router();
 
 router.route('/').post(isAuthenticated, createCourse)
+router.route('/').get(isAuthenticated,getCreatorAllCourse)
 
 export default router;
