531b8c9eedf8bc7721905b2b2ccb3033a82b02f4
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-05 22:55:57 +0530

    added courseId and put req  and also added editCourse

--- backend/routes/courseRoute.js
+++ backend/routes/courseRoute.js
@@ -1,11 +1,14 @@
 
 import express from 'express'
 import isAuthenticated from '../server/middleware/isAuthenticated.js';
-import { createCourse, getCreatorAllCourse } from '../server/controllers/courseController.js';
+import { createCourse, editCourse, getCourseById, getCreatorAllCourse } from '../server/controllers/courseController.js';
+import upload from '../utils/multer.js';
 
 const router = express.Router();
 
 router.route('/').post(isAuthenticated, createCourse)
 router.route('/').get(isAuthenticated,getCreatorAllCourse)
+router.route('/:courseId').put(isAuthenticated,upload.single("courseThumbnail"),editCourse)
+router.route('/:courseId').get(isAuthenticated,getCourseById);
 
 export default router;
