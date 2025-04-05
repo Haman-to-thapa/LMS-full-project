8d62d553f69108c3ca97892b31a65be22f70faae
Haman-to-thapa <hamanthapa0000@gamil.com>
2025-04-05 22:48:33 +0530

    user api fixed some changes to deloying api in render

--- frontend/src/featureSlice/api/authApi.js
+++ frontend/src/featureSlice/api/authApi.js
@@ -1,8 +1,8 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { userLoggedIn, userLoggedOut } from './authSlice'; 
 
-const USER_API = `${import.meta.env.VITE_API_URL}/user/`;
-console.log(import.meta.env.VITE_API_URL);
+const USER_API = "http://localhost:8080/api/v1/user"; 
+
 
 
 
